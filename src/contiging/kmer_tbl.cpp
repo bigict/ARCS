9a6fc5caffffd2c1a12a01c49c8ae12812ebe344
chungongyu <chungongyu@gmail.com>
2015-07-24 13:57:01 +0800

    kmer table

--- src/contiging/kmer_tbl.cpp
+++ src/contiging/kmer_tbl.cpp
@@ -1,1 +1,27 @@
 #include "kmer_tbl.h"
+#include "kseq.h"
+
+#include <boost/format.hpp>
+
+#include <log4cxx/logger.h>
+
+static log4cxx::LoggerPtr logger(log4cxx::Logger::getLogger("contiging.kmer_tbl"));
+
+bool KmerTable::read(std::istream& stream) {
+    if (!stream) {
+        LOG4CXX_WARN(logger, boost::format("invalid input stream"));
+        return false;
+    }
+
+    LOG4CXX_DEBUG(logger, boost::format("construct kmer table begin"));
+
+    DNASeqReader reader(stream);
+
+    DNASeq seq;
+    while (reader.read(seq)) {
+    }
+
+    LOG4CXX_DEBUG(logger, boost::format("construct kmer table end"));
+
+    return true;
+}
--- src/contiging/kmer_tbl.h
+++ src/contiging/kmer_tbl.h
@@ -1,6 +1,7 @@
 #ifndef kmer_tbl_h_
 #define kmer_tbl_h_
 
+#include <iostream>
 #include <string>
 
 #include <tr1/unordered_map>
@@ -10,6 +11,7 @@ public:
     KmerTable();
     virtual ~KmerTable();
 
+    bool read(std::istream& stream);
 private:
     std::tr1::unordered_map<std::string, size_t> _hash_tbl;    
 };
