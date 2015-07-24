53d54ac15fe6c81a4995084b4f8bf8e9aca5d0c6
chungongyu <chungongyu@gmail.com>
2015-07-24 13:17:43 +0800

    kmer impl

--- src/contiging/kmer.cpp
+++ src/contiging/kmer.cpp
@@ -1,22 +1,57 @@
 #include "kmer.h"
 
-Kmer::Kmer() : _data(0) {
+Kmer::Kmer() : _data(0), _length(0) {
 }
 
-Kmer::Kmer(const std::string& seq) : _data(0) {
-	sequence(seq);
+Kmer::Kmer(const std::string& seq) : _data(0), _length(0) {
+    sequence(seq);
 }
 
-Kmer::Kmer(const std::string& seq, size_t i, size_t j) : _data(0) {
-	sequence(seq, i, j);
+Kmer::Kmer(const std::string& seq, size_t i, size_t j) : _data(0), _length(0) {
+    sequence(seq, i, j);
 }
 
 Kmer::~Kmer() {
 }
 
 const std::string Kmer::sequence() const {
-	return "";
+    std::string seq;
+
+    for (size_t i = 0; i < _length; ++i) {
+        int code = (_data >> ((_length - i - 1) * 2)) && 0x03;
+        seq += Nucleotide::code2char(code);
+    }
+
+    return seq;
 }
 
 void Kmer::sequence(const std::string& seq, size_t i, size_t j) {
+    j = (j == std::string::npos || j >= seq.length()) ? seq.length() : j;
+
+    _data = 0;
+    _length = 0;
+
+    while (i < j) {
+        int code = Nucleotide::char2code(seq[i]);
+        _data  = (_data << 2) + code;
+        _length += 1;
+        ++i;
+    }
+}
+
+Kmer& Kmer::operator = (const std::string& seq) {
+    sequence(seq);
+    return *this;
+}
+
+Kmer& Kmer::operator = (const Kmer& o) {
+    if (&o != this) {
+        _data = o._data;
+        _length = o._length;
+    }
+    return *this;
+}
+
+bool Kmer::operator == (const Kmer& o) const {
+    return _data == o._data && _length == o._length;
 }
--- src/contiging/kmer.h
+++ src/contiging/kmer.h
@@ -5,6 +5,37 @@
 
 #include <boost/multiprecision/cpp_int.hpp>
 
+class Nucleotide {
+public:
+    enum {
+        Adenine  = 0x00, 
+        Cytosine = 0x01, 
+        Guanine  = 0x10, 
+        Thymine  = 0x11
+    };
+    
+    static int char2code(char chr) {
+        chr = std::toupper(chr);
+        switch (chr) {
+        case 'A': return Adenine;
+        case 'C': return Cytosine;
+        case 'G': return Guanine;
+        case 'T': return Thymine;
+        }
+        return Adenine;
+    }
+    
+    static char code2char(int code) {
+        switch (code) {
+        case Adenine:  return 'A';
+        case Cytosine: return 'C';
+        case Guanine:  return 'G';
+        case Thymine:  return 'T';
+        }
+        return 'A';
+    }
+};
+
 class Kmer {
 public:
     Kmer();
@@ -20,15 +51,18 @@ public:
         sequence(seq, 0, std::string::npos);
     }
     void sequence(const std::string& seq, size_t i, size_t j);
+    size_t length() const { return _length; }
 
     Kmer& operator = (const std::string& sequence);
-    bool operator == (const Kmer& o);
-    bool operator < (const Kmer& o);
-    bool operator > (const Kmer& o);
-    bool operator != (const Kmer& o);
+    Kmer& operator = (const Kmer& o);
+    bool operator == (const Kmer& o) const;
+    bool operator < (const Kmer& o) const;
+    bool operator > (const Kmer& o) const;
+    bool operator != (const Kmer& o) const;
 
 private:
     boost::multiprecision::cpp_int _data;
+    size_t _length;
 };
 
 #endif // kmer_h_
