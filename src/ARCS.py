73b9fc62f1f7e0d02294bd0bf6e50979b6836830
chungongyu <chungongyu@gmail.com>
2015-10-20 19:00:52 +0800

    ARCS

--- src/ARCS.py
+++ src/ARCS.py
@@ -218,7 +218,7 @@ for i, library in enumerate(config['library_list']):
     # arcs solveLP
     #
     ###################################################################
-    args = '-i %s -o %s' % (os.path.join(config['workspace'], 'position_lp_%d.math' % (i)), os.path.join(config['workspace'], 'edge_cluster_pos_%d' % (i)))
+    args = '-s %s -i %s -o %s' % (os.path.join(config['workspace'], 'scaffold_parameter_0'), os.path.join(config['workspace'], 'position_lp_%d.math' % (i)), os.path.join(config['workspace'], 'edge_cluster_pos_%d' % (i)))
     command_run(ARCS_CMD, 'solveLP', args, config)
 
     ###################################################################
