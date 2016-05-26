4a7312eea9b90f20bf3370215894eabeda9ca1ea
wangbing <wangbing@ict.ac.cn>
2016-05-26 14:31:32 +0800

    out scaffold with 60 base one line

--- src/gap_filler.cpp
+++ src/gap_filler.cpp
@@ -369,7 +369,9 @@ std::ostream& operator<<(std::ostream& os, const GapFiller &obj) {
             seq += obj._uniq_graph._indexer[contigs[j]].seq;
         }
         os << boost::format(">scaf_%d_%d\n") % scf_count++ % seq.size();
-        os << seq << '\n';
+        for(size_t start = 0; start < seq.length(); start+=60) {
+            os << seq.substr(start, 60) << "\n";
+        }
         scaffold_length_list.push_back(seq.length());
     }
     //BOOST_ASSERT(obj._scaffolds.size() ==  scaffold_length_list.size());
